6e604b75cfe8984e822e1be21792caa5991d6e40
Raken <rakentitus@gmail.com>
2019-10-31 21:33:58 +0000

    Check in

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -1,10 +1,10 @@
 name 'nginx'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Titus'
+maintainer_email 'titus.tony@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures nginx'
 long_description 'Installs/Configures nginx'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -5,5 +5,18 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
 #A
-include_recipe 'install'
-#package 'nginx'
+#include_recipe 'install'
+package 'nginx'
+
+service 'nginx' do
+ action [ :enable, :start ]
+end
+
+file '/home/dev1/helloJ' do
+ action :create
+ group 'root'
+ user 'root'
+ mode '777'
+ content "Hello Lord"
+ #not_if { ::File.exist?('home/dev1/helloJ')}
+end
